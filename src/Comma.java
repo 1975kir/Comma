98dac3048065748f09453d12035cd3d53e30fd4c
1975kir <km1975@yandex.ru>
2022-03-08 18:28:46 +0300

    Определение типа переменных

--- src/Comma.java
+++ src/Comma.java
@@ -1,7 +1,8 @@
+// применение запятых в определении цикла for
 public class Comma {
     public static void main(String[] args) {
-        int i,j;
-        for (i = 0, j = 10; i < j; i++, j--)
+//        int i,j;                                //определение типа переменных может быть выполнено как вне,
+        for (int i = 0, j = 10; i < j; i++, j--) // так и в теле цикла
             System.out.println("i и j: " + i + " " + j);
 
     }
